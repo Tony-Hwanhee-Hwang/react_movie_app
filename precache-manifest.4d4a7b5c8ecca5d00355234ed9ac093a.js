5e840800bf141cc90ce896fb9b7aa2ce8a22c5c0
Tony.Hwnag <tony.hwang.developer@gmail.com>
2020-04-01 00:06:23 -0400

    Updates

---
+++ precache-manifest.4d4a7b5c8ecca5d00355234ed9ac093a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6424ba115f2b1b00ce4445db0d6dd7e0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "99d226b788b44d215f0d",
+    "url": "/movie_app/static/css/main.2531ac8e.chunk.css"
+  },
+  {
+    "revision": "7f0d9d91e15441d4b836",
+    "url": "/movie_app/static/js/2.3764c1b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.3764c1b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d226b788b44d215f0d",
+    "url": "/movie_app/static/js/main.c92ddd9a.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
